6d43cec25670670f25df5751ed43b4bfc5bb9919
Woon Seng Hong <88629349+pendragons-code@users.noreply.github.com>
2022-12-17 03:50:20 +0800

    Add files via upload

---
+++ index.js
@@ -0,0 +1,11 @@
+const env = require("dotenv").config()
+const { loadHelper } = require("./src/loaders/loadHelpers.js")
+const { loadEvents } = require("./src/loaders/events.js")
+const { loadMessageCommands } = require("./src/loaders/messageCommands.js")
+const { ShadeBot } = require("./src/loaders/ShadeBot")
+loadEvents()
+loadHelper()
+loadMessageCommands()
+ShadeBot.login(process.env.token)
+console.log(`Hanging out at process: ${process.pid}`)
+process.traceDeprecation = true
